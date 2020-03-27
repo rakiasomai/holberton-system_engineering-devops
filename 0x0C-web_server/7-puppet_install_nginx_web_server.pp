d8af9da94a82a4de9d91624c58025b4d9535eca3
rakiasomai <1069@holbertonschool.com>
2020-03-27 18:31:20 +0000

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,27 @@
 # Install ngnix with puppet and configurate a server
+# Requirements:
+#    Nginx should be listening on port 80
+#    When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Holberton School
+#    The redirection must be a 301 Moved Permanently
 
-exec {'apde':
+exec {'update':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
   command  => 'sudo apt-get -y update',
 }
 
-exec {'instal':
+exec {'install':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
   command  => 'sudo apt-get -y install nginx',
 }
 
-exec {'index':
+exec {'html':
   provider => shell,
   command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
-exec {'Config':
+exec {'sedConfig':
   provider => shell,
   command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
 }
