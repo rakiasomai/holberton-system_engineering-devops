9eb7c304c487b60744628f2f1b48bb11f55cc5b9
rakiasomai <1069@holbertonschool.com>
2020-03-27 18:16:22 +0000

    commit

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,23 @@
+# Install ngnix with puppet and configurate a server
+
+exec {'apd':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+}
+
+
+exec {'html':
+  provider => shell,
+  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+}
+
+exec {'Config':
+  provider => shell,
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+}
+
+exec {'start':
+  provider => shell,
+  command  => 'sudo service nginx start',
+}
