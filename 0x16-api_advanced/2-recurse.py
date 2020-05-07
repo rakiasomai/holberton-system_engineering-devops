143a7cf6639ae3a36cc56b8b38b8cdde6f591f63
rakiasomai <1069@holbertonschool.com>
2020-05-07 17:14:13 +0000

    task2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''''This module contain the function number_of_subscribers'''
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], then=""):
+    ''' def recurse '''
+    if (then is None):
+        return hot_list
+
+    if (len(hot_list) == 0):
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+            subreddit, after)
+    user = {'user-agent': 'rakia'}
+
+    r = requests.get(url, headers=user)
+    if (r.status_code is 404):
+        return None
+    elif 'data' not in r.json():
+        return None
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            hot_list.append(post['data']['title'])
+
+    then = r['data']['then']
+    return recurse(subreddit, hot_list, then)
