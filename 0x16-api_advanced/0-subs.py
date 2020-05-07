8a3c70612e9af824a5f6a1e7b3363862d0503535
rakiasomai <1069@holbertonschool.com>
2020-05-07 16:56:59 +0000

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    ''' def number of subscribers'''
-    get_url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'philsrequest'}
-    y = requests.get(get_url, headers=headers)
-    if (y.status_code is 404 or y.status_code is 302):
+    ''' def number of subscribers '''
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'rakiarequest'}
+    r = requests.get(url, headers=headers)
+    if (r.status_code is 302 or r.status_code is 404):
         return 0
-    y = y.json()
-    if ('error' in y):
+    r = r.json()
+    if ('error' in r):
         return 0
-    elif ('subscribers' in y['data']):
-        return y['data']['subscribers']
+    elif ('subscribers' in r['data']):
+        return r['data']['subscribers']
     else:
         return 0
