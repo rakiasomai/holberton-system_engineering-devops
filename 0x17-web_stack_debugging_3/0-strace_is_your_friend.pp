dcf03d00cf6d8c83047fb1ece636dacb18323c73
rakiasomai <1069@holbertonschool.com>
2020-05-20 15:19:20 +0000

    one

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# fixing WP setting issues
+exec { 'fixing WP setting issues':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
