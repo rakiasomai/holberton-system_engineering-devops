a5eba0f02b3943853c2f96847072b6f70a58c28e
rakiasomai <1069@holbertonschool.com>
2020-05-28 18:13:54 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
-# fix our stack so that we get to 0
-exec {'number':
-provider => shell,
-path    => ['/usr/bin', '/bin'],
-command => "sudo sed -i 's/15/3000/g'  /etc/default/nginx;  sudo service nginx restart",
+# Ngnix : fix the number of req
+exec { 'number file':
+  provider => shell,
+  path     => ['/usr/bin', '/bin'],
+  command  => "sudo sed -i 's/15/3000/g'  /etc/default/nginx;  sudo service nginx restart",
 }
